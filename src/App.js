43c50e2b54fbfcd40b45db3dc41febf39616626f
vishal sangole <wishalsangole4444@gmail.com>
2022-03-06 20:39:23 +0530

    hearder component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {Routes , Route} from "react-router-dom";
 import './App.css';
 import { Homepage } from './page/homepage/Homepage.component';
 import Shop from './page/shoppage/Shop.component';
+import { Header } from './components/header/Header.component';
 
 
 function App() {
   return (
     <div >
+      <Header/>
       <Routes>
         <Route path='/' element={<Homepage/>}/>
         <Route path='/shop' element={<Shop/>}/>
